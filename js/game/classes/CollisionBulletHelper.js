187af124bc3bac7d82ab42a7728e9d31fb791423
Boris Ermachenko <ermachenkoboris@gmail.com>
2022-11-02 18:21:47 +0200

    [game-part]: fix redirect rull

--- js/game/classes/CollisionBulletHelper.js
+++ js/game/classes/CollisionBulletHelper.js
@@ -1,30 +1,45 @@
 export class CollisionBulletHepler {
-
   fixCollision(enemy, enemies, projectile, enemyIndex, projectileIndex) {
     projectile.splice(projectileIndex, 1);
     enemy.damaged();
     if (enemy.health <= 0) {
-    window.location.href = `/${enemy.page}`
+      window.location.assign(`/${enemy.page}`);
       enemies.splice(enemyIndex, 1);
     }
   }
 
   checkCollisionAndFix(enemies, projectiles) {
     enemies.forEach((enemy, enemyIndex) => {
-        projectiles.forEach((projectile, projectileIndex) => {
-            this.checkCollision(enemies, projectiles, enemy, enemyIndex, projectile, projectileIndex)
-        });
+      projectiles.forEach((projectile, projectileIndex) => {
+        this.checkCollision(
+          enemies,
+          projectiles,
+          enemy,
+          enemyIndex,
+          projectile,
+          projectileIndex
+        );
+      });
     });
   }
 
-  checkCollision(enemies, projectiles, enemy, enemyIndex, projectile, projectileIndex) {
-        const dist = Math.hypot(enemy.x - projectile.x, enemy.y - projectile.y);
-        if (dist <= enemy.radious + projectile.radious) {
-          this.fixCollision(enemy, enemies, projectiles, enemyIndex, projectileIndex);
-        }
-
+  checkCollision(
+    enemies,
+    projectiles,
+    enemy,
+    enemyIndex,
+    projectile,
+    projectileIndex
+  ) {
+    const dist = Math.hypot(enemy.x - projectile.x, enemy.y - projectile.y);
+    if (dist <= enemy.radious + projectile.radious) {
+      this.fixCollision(
+        enemy,
+        enemies,
+        projectiles,
+        enemyIndex,
+        projectileIndex
+      );
+    }
   }
-
-
-
 }
